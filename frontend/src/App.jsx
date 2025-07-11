eb9b99464968d17ee6abf0948eae5d0299b7130c
Karyna Pomin <115844791+KarynaPomin@users.noreply.github.com>
2025-07-11 15:09:23 +0200

    Create folder for css where imported app.css and index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './css/App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar';
 import Home from "./pages/Home"
